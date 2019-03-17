49f3d880a54bbc80fe034dbab39f03477157c1f0
sasalx <oyundanben@gmail.com>
2019-03-18 02:42:15 +0300

    Query Patient Data Test

--- test/app.test.ts
+++ test/app.test.ts
@@ -17,3 +17,27 @@ describe("Connection", () => {
     expect(response.status).toBe(200);
   });
 });
+
+describe("Query should", () => {
+  test("return a Patient", async () => {
+    // ID LwUY6z3pTql0VbQemjBh is the Test Patient
+    const response = await axios.post("http://localhost:4000/", {
+      query: `
+        query{
+          getPatient(
+            id: "LwUY6z3pTql0VbQemjBh"
+          ) {
+            name
+          }
+        }
+      `
+    });
+    expect(response.data).toMatchObject({
+      data: {
+        getPatient: {
+          name: 'Test'
+        }
+      }
+    });
+  });
+});
